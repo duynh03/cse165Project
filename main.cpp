404763539be071fbc6e9446fa1c983c396520f22
duynh03 <therealphathuynh2@gmail.com>
2024-04-03 20:03:05 -0700

    changed dot size to 1 pixel removed commented code

--- main.cpp
+++ main.cpp
@@ -4,34 +4,6 @@
 using namespace std;
 //g++ -o run main.cpp -lGL -lglut
 //------------------------------------------------------------------------------
-
-// void render(void){
-//     glClear(GL_COLOR_BUFFER_BIT);
-
-//     glBegin(GL_TRIANGLES);
-//     glColor3f(1.0, 0.0, 0.0); //red
-//     glVertex2f(-0.5, 1.0);
-//     glColor3f(0.0, 1.0, 0.0); // Green
-//     glVertex2f(-1.0, 0.0);
-//     glColor3f(0.0, 0.0, 1.0); // Blue
-//     glVertex2f(0.0, 0.0);
-//     glEnd();
-//     glFlush();
-// }
-
-
-// int main(int argc, char** argv) {
-//     glutInit(&argc, argv);
-//     glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
-//     glutInitWindowSize(500, 500);
-//     glutInitWindowPosition(100, 100);
-//     glutCreateWindow("OpenGL Window");
-//     glClearColor(0.0, 0.0, 0.0, 1.0);
-//     glutDisplayFunc(render);
-//     glutMainLoop();
-//     return 0;
-// }
-
 int mouseX = 0;
 int mouseY = 0;
 
@@ -56,7 +28,7 @@ void render() {
     // Example: Draw a point at the current mouse position
     glEnable(GL_POINT);
     glColor3f(1.0, 0.0, 0.0); // Red color
-    glPointSize(5.0); // Set point size
+    glPointSize(1.0); // Set point size
     glBegin(GL_POINTS);
     glVertex2f((mouseX / (float)glutGet(GLUT_WINDOW_WIDTH)) * 2 - 1, -(mouseY / (float)glutGet(GLUT_WINDOW_HEIGHT)) * 2 + 1); // Normalize mouse coordinates
     glVertex2f(.5, .5);
