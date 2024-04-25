08bda7875927bacc1eac827647623ae114f4a4cd
duynh03 <therealphathuynh2@gmail.com>
2024-04-25 15:39:05 -0700

    a random point now  appear whenever the mouse move

--- main.cpp
+++ main.cpp
@@ -8,11 +8,11 @@ using namespace std;
 int mouseX = 0;
 int mouseY = 0;
 
-double randomGen(){
+float randomGen(){
     random_device rd;
     mt19937 eng(rd());
     uniform_real_distribution<> distr(-1.0, 1.0);
-    double random_num = distr(eng);
+    float random_num = distr(eng);
     return random_num;   
 }
 
@@ -23,10 +23,10 @@ void passiveMotion(int x, int y) {
     
     // Print mouse coordinates to console
     std::cout << "Mouse Position: (" << mouseX << ", " << mouseY << ")" << std::endl;
-    cout << "Window width: "  << glutGet(GLUT_WINDOW_WIDTH) << endl;
-    cout << "window Height: " << glutGet(GLUT_WINDOW_HEIGHT) << endl;
     cout << "Mousex: " << (mouseX / (float)glutGet(GLUT_WINDOW_WIDTH)) * 2 - 1 << endl;
     cout << "MouseY: " << -(mouseY / (float)glutGet(GLUT_WINDOW_HEIGHT)) * 2 + 1 << endl;
+
+    //Redraw the window
     glutPostRedisplay(); //refresh where the cursor is so it can be redrawn in the window
 }
 
@@ -34,7 +34,9 @@ void triangle() {
     glClear(GL_COLOR_BUFFER_BIT);
     float xCoordinate = (mouseX / (float)glutGet(GLUT_WINDOW_WIDTH)) * 2 - 1;
     float yCoordinate = -(mouseY /  (float)glutGet(GLUT_WINDOW_HEIGHT)) * 2 + 1;
- 
+    float X = randomGen();
+    float Y = randomGen();
+
     glEnable(GL_TRIANGLES);
     glBegin(GL_TRIANGLES);
         glColor3f(1.0, 0.0, 0.0); // Green Color
@@ -44,20 +46,14 @@ void triangle() {
         glColor3f(1.0, 0.0, 0.0);
         glVertex2f(xCoordinate + 0.05, yCoordinate - 0.05);
     glEnd();
-    glutSwapBuffers();
-}
 
-void testpoint(){
-    // glClear(GL_COLOR_BUFFER_BIT);  //if this isnt here, then the dots dont get cleared
-    float xCoordinate = (mouseX / (float)glutGet(GLUT_WINDOW_WIDTH)) * 2 - 1;
-    float yCoordinate = -(mouseY /  (float)glutGet(GLUT_WINDOW_HEIGHT)) * 2 + 1;    
     glEnable(GL_POINT);
-    glPointSize(5.0); //set size THEN begin, otherwise nothing show on screen
+    glPointSize(5.0);
     glBegin(GL_POINTS);
-    glColor3f(1.0, 0.0, 0.0);
-    // glVertex2f(0.5, 0.5);
-    glVertex2d(xCoordinate + 0.1, yCoordinate + 0.1);
+        glColor3f(1.0 ,0.0, 0.0);
+        glVertex2f(X, Y);
     glEnd();
+
     glutSwapBuffers();
 }
 
@@ -68,7 +64,7 @@ int main(int argc, char** argv) {
     glutCreateWindow("OpenGL Window");
     
     glutPassiveMotionFunc(passiveMotion); // Register passive motion callback
-i
+    glutDisplayFunc(triangle);
     glutMainLoop();
     return 0;
 }
