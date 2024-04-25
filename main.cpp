cb0fd70bab391037e867e8b43fdea27d9b2e76a6
duynh03 <therealphathuynh2@gmail.com>
2024-04-25 14:53:24 -0700

    added random number genrerator

--- main.cpp
+++ main.cpp
@@ -1,13 +1,20 @@
 #include <GL/gl.h>
 #include <GL/freeglut.h>
 #include <iostream>
-#include <cstdlib>
-#include <ctime>
+#include <random>
 using namespace std;
 //g++ -o run main.cpp -lGL -lglut
 //------------------------------------------------------------------------------
 int mouseX = 0;
 int mouseY = 0;
+
+double randomGen(){
+    random_device rd;
+    mt19937 eng(rd());
+    uniform_real_distribution<> distr(-1.0, 1.0);
+    double random_num = distr(eng);
+    return random_num;   
+}
 
 void passiveMotion(int x, int y) {
     // Update mouse coordinates
@@ -23,7 +30,7 @@ void passiveMotion(int x, int y) {
     glutPostRedisplay(); //refresh where the cursor is so it can be redrawn in the window
 }
 
-void render() {
+void triangle() {
     glClear(GL_COLOR_BUFFER_BIT);
     float xCoordinate = (mouseX / (float)glutGet(GLUT_WINDOW_WIDTH)) * 2 - 1;
     float yCoordinate = -(mouseY /  (float)glutGet(GLUT_WINDOW_HEIGHT)) * 2 + 1;
@@ -37,20 +44,19 @@ void render() {
         glColor3f(1.0, 0.0, 0.0);
         glVertex2f(xCoordinate + 0.05, yCoordinate - 0.05);
     glEnd();
-    
     glutSwapBuffers();
 }
 
 void testpoint(){
     // glClear(GL_COLOR_BUFFER_BIT);  //if this isnt here, then the dots dont get cleared
+    float xCoordinate = (mouseX / (float)glutGet(GLUT_WINDOW_WIDTH)) * 2 - 1;
+    float yCoordinate = -(mouseY /  (float)glutGet(GLUT_WINDOW_HEIGHT)) * 2 + 1;    
     glEnable(GL_POINT);
     glPointSize(5.0); //set size THEN begin, otherwise nothing show on screen
-    float xCoordinate = (mouseX / (float)glutGet(GLUT_WINDOW_WIDTH)) * 2 - 1;
-    float yCoordinate = -(mouseY /  (float)glutGet(GLUT_WINDOW_HEIGHT)) * 2 + 1;
     glBegin(GL_POINTS);
     glColor3f(1.0, 0.0, 0.0);
     // glVertex2f(0.5, 0.5);
-    glVertex2d(xCoordinate, yCoordinate);
+    glVertex2d(xCoordinate + 0.1, yCoordinate + 0.1);
     glEnd();
     glutSwapBuffers();
 }
@@ -62,8 +68,7 @@ int main(int argc, char** argv) {
     glutCreateWindow("OpenGL Window");
     
     glutPassiveMotionFunc(passiveMotion); // Register passive motion callback
-    // glutDisplayFunc(render);
-    glutDisplayFunc(testpoint);
+i
     glutMainLoop();
     return 0;
 }
